573deae4b51a803ad58ce200e191080fd14ea90a
Robert-Stefan <robertboss05@hotmail.it>
2021-06-10 18:52:52 +0200

    Add seeder

--- app/Plan.php
+++ app/Plan.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Viaggio extends Model
+class Plan extends Model
 {
     //
 }
--- database/seeds/ViaggiTableSeeder.php
+++ database/seeds/ViaggiTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Plan;
 
 class ViaggiTableSeeder extends Seeder
 {
@@ -11,6 +12,19 @@ class ViaggiTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i = 0; $i < 20; $i++) {
+            // Creazione istanza (record, row)
+            $new_viaggio = new Plan();
+
+            // Popolazione colonne
+            $new_viaggio->title = 'Viaggio a Verona';
+            $new_viaggio->description = 'Verona, capoluogo di provincia del Veneto, si trova nella pianura allo sbocco della valle dell\'Adige, ai piedi dei monti Lessini. Antico insediamento paleoveneto, la città divenne ricca e potente con i Romani, che ne apprezzarono la naturale posizione strategica, punto di passaggio verso il nord dell\'Europa.';
+            $new_viaggio->days = rand(1, 10);
+            $new_viaggio->price = rand(100, 1000);
+            $new_viaggio->energy_rating = 'A';
+
+            // Salvare record a db  
+            $new_viaggio->save();
+        }
     }
 }
